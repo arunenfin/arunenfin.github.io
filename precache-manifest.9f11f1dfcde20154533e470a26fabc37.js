ee3335896f754058a1e3be5e2a07a37c77bd1747
Arun Devarajan <arun.d@enfintechnologies.com>
2020-04-27 14:42:08 +0530

    deploy react build

---
+++ precache-manifest.9f11f1dfcde20154533e470a26fabc37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2adfed0fcbecdc5674cf59f8d4ad0df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d99cb644b01dd38ea0d4",
+    "url": "/static/css/2.3beecd4b.chunk.css"
+  },
+  {
+    "revision": "8b43d6023deb65bba412",
+    "url": "/static/css/main.319aabc5.chunk.css"
+  },
+  {
+    "revision": "d99cb644b01dd38ea0d4",
+    "url": "/static/js/2.fc58c474.chunk.js"
+  },
+  {
+    "revision": "af0a063b7aacc833132980c2224e7d2e",
+    "url": "/static/js/2.fc58c474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b43d6023deb65bba412",
+    "url": "/static/js/main.ad976f64.chunk.js"
+  },
+  {
+    "revision": "54f7a4f89c8c5aa94300",
+    "url": "/static/js/runtime-main.622a73a4.js"
+  }
+]);
